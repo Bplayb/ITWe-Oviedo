09b447a31e50cce5883fc8f3958d020b94655779
= <=>
2024-12-05 14:19:54 +0100

    Stetic addings to the home page, other minor fixes.

--- components/footer.js
+++ components/footer.js
@@ -15,7 +15,6 @@ class Footer extends HTMLElement {
             <footer>
                 <nav class="footer">
                     <ul>
-                        <li><a href="home.html">Home</a></li>
                         <li><a href="https://www.oviedo.es/noticias">News</a></li>
                         <li><a href="https://www.facebook.com/www.oviedo.es/">Facebook</a></li>
                         <li><a href="https://x.com/aytoviedo">Twitter</a></li>
